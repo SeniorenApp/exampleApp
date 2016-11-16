8d3c46487343c5bcca2709c11cc9db8b87973f6e
SeniorenApp <seniorenapp@gmx.net>
2016-11-16 13:33:18 +0100

    Full release.

---
+++ SeniorenApp/obj/Debug/android/src/md54259e6513023dd8c68a5505c80f1ae41/MainActivity.java
@@ -0,0 +1,56 @@
+package md54259e6513023dd8c68a5505c80f1ae41;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onNewIntent:(Landroid/content/Intent;)V:GetOnNewIntent_Landroid_content_Intent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("SeniorenApp.MainActivity, SeniorenApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("SeniorenApp.MainActivity, SeniorenApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onNewIntent (android.content.Intent p0)
+	{
+		n_onNewIntent (p0);
+	}
+
+	private native void n_onNewIntent (android.content.Intent p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ SeniorenApp/obj/Release/android/src/md58c97c81a243b85338327767ca7289695/Receiver.java
@@ -0,0 +1,47 @@
+package md58c97c81a243b85338327767ca7289695;
+
+
+public class Receiver
+	extends android.content.BroadcastReceiver
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onReceive:(Landroid/content/Context;Landroid/content/Intent;)V:GetOnReceive_Landroid_content_Context_Landroid_content_Intent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("SeniorenApp.USB.Receiver, SeniorenApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Receiver.class, __md_methods);
+	}
+
+
+	public Receiver () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == Receiver.class)
+			mono.android.TypeManager.Activate ("SeniorenApp.USB.Receiver, SeniorenApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onReceive (android.content.Context p0, android.content.Intent p1)
+	{
+		n_onReceive (p0, p1);
+	}
+
+	private native void n_onReceive (android.content.Context p0, android.content.Intent p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
