e1ea60d32c1f86dd4c8b1728d9720f6b1681e1f0
SeniorenApp <seniorenapp@gmx.net>
2016-11-16 19:45:39 +0100

    New Version with error catching.

--- SeniorenApp/MainActivity.cs
+++ SeniorenApp/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.Hardware.Usb;
 using Android.OS;
 using Android.Widget;
 using Java.IO;
+using System;
 
 namespace SeniorenApp
 {
@@ -11,75 +12,84 @@ namespace SeniorenApp
     [Activity(Label = "SeniorenApp", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        TextView textView;
+
         protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-
-            var textView = FindViewById<TextView>(Resource.Id.textView1);
-            textView.Text = "bla";
-
-            //var i = new IntentFilter();
-            //i.AddAction(UsbManager.ActionUsbAccessoryAttached);
-            //i.AddAction(UsbManager.ActionUsbAccessoryDetached);
-            //i.AddAction("USBPERMISSION");
-
-            UsbAccessory usb = (UsbAccessory)Intent.GetParcelableExtra(UsbManager.ExtraAccessory);
-
-            if (usb != null)
+            try
             {
-                textView.Text = "device found";
-            }
-            else
-            {
-                textView.Text = "device not found";
-            }
+                base.OnCreate(bundle);
 
-            if (UsbManager.ActionUsbAccessoryAttached.Equals(Intent.Action))
-            {
-                usb = (UsbAccessory)Intent.GetParcelableExtra(UsbManager.ExtraAccessory);
-                textView.Text = "accessory attached" + usb != null ? " - Could not create Accessory" : " - Accessory created";
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.Main);
+
+                textView = FindViewById<TextView>(Resource.Id.textView1);
+                textView.Text = "bla";
+
+                //var i = new IntentFilter();
+                //i.AddAction(UsbManager.ActionUsbAccessoryAttached);
+                //i.AddAction(UsbManager.ActionUsbAccessoryDetached);
+                //i.AddAction("USBPERMISSION");
+
+                UsbAccessory usb = (UsbAccessory)Intent.GetParcelableExtra(UsbManager.ExtraAccessory);
+
+                if (usb != null)
+                {
+                    textView.Text = "device found";
+                }
+                else
+                {
+                    textView.Text = "device not found";
+                }
+
+                if (UsbManager.ActionUsbAccessoryAttached.Equals(Intent.Action))
+                {
+                    usb = (UsbAccessory)Intent.GetParcelableExtra(UsbManager.ExtraAccessory);
+                    textView.Text = "accessory attached" + usb != null ? " - Could not create Accessory" : " - Accessory created";
+                }
+                else
+                {
+                    textView.Text = "Accessory not attached - Action was:" + Intent.Action;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                textView.Text = "Accessory not attached - Action was:" + Intent.Action;
+                textView.Text = ex.ToString() + System.Environment.NewLine + ex.GetType().ToString() + System.Environment.NewLine + ex.InnerException.ToString();
             }
+            
 
             // RegisterReceiver(new Receiver((UsbManager)GetSystemService(UsbService), OnAccessoryOpened), i);
         }
 
         protected override void OnNewIntent(Intent intent)
         {
-            base.OnNewIntent(intent);
-
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-
-            var textView = FindViewById<TextView>(Resource.Id.textView1);
-            textView.Text = "bla";
-
-            UsbAccessory usb = (UsbAccessory)intent.GetParcelableExtra(UsbManager.ExtraAccessory);
-
-            if (usb != null)
+            try
             {
-                textView.Text = "device found in onnewintent";
-            }
-            else
-            {
-                textView.Text = "device not found in onnewintent";
-            }
+                UsbAccessory usb = (UsbAccessory)intent.GetParcelableExtra(UsbManager.ExtraAccessory);
 
-            if (UsbManager.ActionUsbAccessoryAttached.Equals(Intent.Action))
-            {
-                usb = (UsbAccessory)Intent.GetParcelableExtra(UsbManager.ExtraAccessory);
-                textView.Text = "accessory attached" + usb != null ? " - Could not create Accessory" : " - Accessory created in onnewintent";
+                if (usb != null)
+                {
+                    textView.Text = "device found in onnewintent";
+                }
+                else
+                {
+                    textView.Text = "device not found in onnewintent";
+                }
+
+                if (UsbManager.ActionUsbAccessoryAttached.Equals(Intent.Action))
+                {
+                    usb = (UsbAccessory)Intent.GetParcelableExtra(UsbManager.ExtraAccessory);
+                    textView.Text = "accessory attached" + usb != null ? " - Could not create Accessory" : " - Accessory created in onnewintent";
+                }
+                else
+                {
+                    textView.Text = "Accessory not attached - Action was:" + Intent.Action + " in onnewintent";
+                }
             }
-            else
+            catch (Exception ex)
             {
-                textView.Text = "Accessory not attached - Action was:" + Intent.Action + " in onnewintent";
-            }
+                textView.Text = ex.ToString() + System.Environment.NewLine + ex.GetType().ToString() + System.Environment.NewLine + ex.InnerException.ToString();
+            }          
         }
 
         public void OnAccessoryOpened(FileInputStream stream)
